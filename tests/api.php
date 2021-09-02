0e78e815f27b35285f5489f3c0e8ef4128d7b7b8
Kyour.cn <kyour@vip.qq.com>
2021-09-02 16:42:59 +0800

    删除文件支持批量操作
    多个文件通过,分割，遍历删除

--- tests/api.php
+++ tests/api.php
@@ -1,65 +1,60 @@
 <?php
 /**
+ * 此文件是Sword/Storage的api接口
  * 接口配置信息
- * 请修改信息才可使用，出于安全考虑
  */
 $config = [
-    'User' => 'user',
-    'Secret' => 'xxx'
+    'User' => 'school',
+    'Secret' => 'kyourpwd'
 ];
 
-/**
- * 程序部分 =.= 若无特殊需要，请勿修改
- */
 
 //显示所有错误信息
 // ini_set("display_errors", "On");
 // error_reporting(E_ALL);
 // ini_set("display_errors", "On");
-// error_reporting(E_ALL);
+// error_reporting(E_ALL); //显示所有错误信息
 
-// 安全验证 -默认配置
-if($config['User'] == 'user' and $config['User'] == 'xxx'){
-    echo json_encode(['code' => 100, 'message' => '请修改默认的 $config 配置内容，避免发生安全事故！']);
+if(empty($_POST['Action'])){
+    echo  json_encode(['code' => 100]);
     return;
 }
 
-if (empty($_POST['Action'])) {
-    echo json_encode(['code' => 100]);
+if($_POST['User'] != $config['User'] or $_POST['Secret'] != $config['Secret']){
+    echo  json_encode(['code' => 101]);
     return;
 }
 
-if ($_POST['User'] != $config['User'] or $_POST['Secret'] != $config['Secret']) {
-    echo json_encode(['code' => 101]);
-    return;
-}
-
-if ($_POST['Action'] == 'upload' and isset($_FILES["file"])) {
-    if ($_FILES["file"]["error"] > 0) {
-        // echo "错误：" . $_FILES["file"]["error"] . "<br>";
+if($_POST['Action'] == 'upload' and isset($_FILES["file"])){
+    if($_FILES["file"]["error"] > 0){
         return json_encode(['code' => 1, 'message' => $_FILES["file"]["error"]]);
-    } else {
+    }else{
         $target = $_POST['Path'];
         $index = strrpos($target, '/');
 
         //保存到Public目录下
-        $dir = './' . substr($target, 0, $index);
+        $dir = './'.substr($target, 0, $index);
         //文件夹创建
-        if (!file_exists($dir)) {
-            mkdir($dir, 0777, true);
+        if (!file_exists($dir)){
+            mkdir($dir,0777,true);
         }
 
-        move_uploaded_file($_FILES["file"]["tmp_name"], $dir . "/" . substr($target, $index + 1));
-
+        move_uploaded_file($_FILES["file"]["tmp_name"], $dir. "/" . substr($target, $index +1));
+    
         echo json_encode(['code' => 0]);
         return;
     }
-} elseif ($_POST['Action'] == 'delete') {
-
-    $target = './' . $_POST['Path'];
-    if (file_exists($target)) {
-        unlink($target);
+}elseif($_POST['Action'] == 'delete'){
+    
+    $path = $_POST['Path'];
+    $files = explode(',', $path);
+    foreach ($files as $file){
+        $target = './' . $file;
+        if(file_exists($target)){
+            unlink($target);
+        }
     }
+
     echo json_encode(['code' => 0]);
     return;
 }
